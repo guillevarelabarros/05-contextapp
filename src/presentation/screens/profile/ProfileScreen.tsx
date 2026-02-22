78df09cd15f3532ddd2c82d1ab527aadcae7d3b2
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-22 11:21:27 -0300

    refactor: restructure app by removing App component and adding Main component; create screens for Home, Profile, and Settings

---
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from '../../../config';
+
+export const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  );
+};
