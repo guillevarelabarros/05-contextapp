144e4df0dc44dbadc134b318ec17e378ba589113
Guillermo I. Varela Barros <guillevarelabarros@gmail.com>
2026-02-22 13:59:16 -0300

    feat: add changeProfile function to profile store and update ProfileScreen to allow profile changes

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,17 +1,35 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { styles } from '../../../config';
 import { useProfileStore } from '../../store/profile-store';
 
 export const ProfileScreen = () => {
-  const { name, email } = useProfileStore();
+  const { name, email, changeProfile } = useProfileStore();
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>ProfileScreen</Text>
       <Text>{name}</Text>
       <Text>{email}</Text>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => changeProfile('Fernando Herrera', email)}>
+        <Text>Cambiar nombre</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => changeProfile(name, 'fernando.herrera@google.com')}>
+        <Text>Cambiar email</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => changeProfile('John Doe', 'john.doe@google.com')}>
+        <Text>Regresar a John Doe</Text>
+      </Pressable>
     </View>
   );
 };
--- src/presentation/store/profile-store.ts
+++ src/presentation/store/profile-store.ts
@@ -3,9 +3,11 @@ import { create } from 'zustand';
 interface ProfileState {
   name: string;
   email: string;
+  changeProfile: ( name: string, email: string ) => void;
 }
 
-export const useProfileStore = create<ProfileState>()( () => ( {
+export const useProfileStore = create<ProfileState>()( ( set ) => ( {
   name: 'John Doe',
   email: 'john.doe@google.com',
+  changeProfile: ( name, email ) => set( { name, email } ),
 } ) );
